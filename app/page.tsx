69e542d43e086f1764be1d5c73ed01d3fbf3096f
Yongjun Park <yopark@student.42seoul.kr>
2024-04-07 00:19:35 +0900

    build(pkg): install prettier-plugin-tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,21 @@ const RootPage = () => {
   return (
     <main
       className={cn(
-        'w-screen h-screen',
-        'flex flex-col justify-center items-center',
+        'h-screen w-screen',
+        'flex flex-col items-center justify-center',
         'bg-neutral-50',
       )}
     >
       <button
         className={cn(
-          'flex flex-col justify-center items-center gap-10',
+          'flex flex-col items-center justify-center gap-10',
           'hover:scale-110',
           'active:scale-100',
           'transition-transform',
         )}
       >
         <p className="text-8xl">👋</p>
-        <p className="font-bold text-4xl">Hello World</p>
+        <p className="text-4xl font-bold">Hello World</p>
       </button>
     </main>
   );
