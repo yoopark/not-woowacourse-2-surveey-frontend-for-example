16c8f43e164b07fe39270d18b3642ea0ee03ac4d
Yongjun Park <yopark@student.42seoul.kr>
2024-04-07 15:56:25 +0900

    chore(next.config.js): add rewrites options

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    const API_URL = 'https://not-woowacourse-api.yopark.dev';
+
+    return [
+      {
+        source: '/backend-api/:path*',
+        destination: `${API_URL}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
